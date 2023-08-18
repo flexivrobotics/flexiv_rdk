6c4ffa6d8452f88201f4f4105f1a206f878eb7bc
Peizhang Zhu <peizhang.zhu@flexiv.com>
2023-08-18 14:29:32 -0700

    Add Log::critical

--- include/flexiv/Gripper.hpp
+++ include/flexiv/Gripper.hpp
@@ -19,35 +19,40 @@ class Gripper
 {
 public:
     /**
-     * @brief Create a flexiv::Gripper instance for gripper control.
+     * @brief [Non-blocking] Create a flexiv::Gripper instance and initialize
+     * robot gripper control.
      * @param[in] robot Reference to the instance of flexiv::Robot.
-     * @throw InitException if the instance failed to initialize.
+     * @throw InitException if the initialization sequence failed.
      */
     Gripper(const Robot& robot);
     virtual ~Gripper();
 
     /**
-     * @brief Grasp with direct force control. Requires the mounted gripper to
-     * support direct force control.
+     * @brief [Blocking] Grasp with direct force control. Requires the mounted
+     * gripper to support direct force control.
      * @param[in] force Target gripping force. Positive: closing force,
      * negative: opening force [N].
      * @note Applicable operation modes: all modes except IDLE.
+     * @throw ExecutionException if error occurred during execution.
      * @warning Target inputs outside the valid range (specified in gripper's
      * configuration file) will be saturated.
-     * @throw ExecutionException if error occurred during execution.
+     * @warning This function blocks until the request is successfully delivered
+     * to the robot.
      */
     void grasp(double force);
 
     /**
-     * @brief Move the gripper fingers with position control.
+     * @brief [Blocking] Move the gripper fingers with position control.
      * @param[in] width Target opening width [m].
      * @param[in] velocity Closing/opening velocity, cannot be 0 [m/s].
      * @param[in] forceLimit Maximum output force during movement [N]. If not
      * specified, default force limit of the mounted gripper will be used.
      * @note Applicable operation modes: all modes except IDLE.
+     * @throw ExecutionException if error occurred during execution.
      * @warning Target inputs outside the valid range (specified in gripper's
      * configuration file) will be saturated.
-     * @throw ExecutionException if error occurred during execution.
+     * @warning This function blocks until the request is successfully delivered
+     * to the robot.
      */
     void move(double width, double velocity, double forceLimit = 0);
 
