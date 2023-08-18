a9a7a6ff962aa379e26c4019df3d2ba187f842c1
Peizhang Zhu <peizhang.zhu@flexiv.com>
2023-08-18 14:30:14 -0700

    Remove gravityEarth input parameter to flexiv::Model constructor, auto-synced from robot server

--- include/flexiv/Model.hpp
+++ include/flexiv/Model.hpp
@@ -22,18 +22,18 @@ class Model
 {
 public:
     /**
-     * @brief Create a flexiv::Model instance to get robot dynamics data.
+     * @brief [Non-blocking] Create a flexiv::Model instance and initialize the
+     * integrated dynamics engine.
      * @param[in] robot Reference to the instance of flexiv::Robot.
-     * @param[in] gravityEarth Earth's gravity vector in base frame. Default
-     * to \f$ [0.0, 0.0, -9.81]^T \f$. Unit: \f$ [m/s^2] \f$.
      * @throw InitException if the instance failed to initialize.
+     * @note The robot mounting position set in Flexiv Elements is automatically
+     * synced over so that this dynamics engine produces the correct result.
      */
-    Model(const Robot& robot,
-        const Eigen::Vector3d& gravityEarth = Eigen::Vector3d(0.0, 0.0, -9.81));
+    Model(const Robot& robot);
     virtual ~Model();
 
     /**
-     * @brief Update robot model using new joint states data.
+     * @brief [Non-blocking] Update robot model using new joint states data.
      * @param[in] positions Current joint positions: \f$ q \in \mathbb{R}^{DOF
      * \times 1} \f$. Unit: \f$ [rad] \f$.
      * @param[in] velocities Current joint velocities: \f$ \dot{q} \in
@@ -44,8 +44,8 @@ public:
         const std::vector<double>& velocities);
 
     /**
-     * @brief Compute and get the Jacobian matrix at the frame of the specified
-     * link \f$ i \f$, expressed in the base frame.
+     * @brief [Non-blocking] Compute and get the Jacobian matrix at the frame of
+     * the specified link \f$ i \f$, expressed in the base frame.
      * @param[in] linkName Name of the link to get Jacobian for.
      * @return Jacobian matrix: \f$ ^{0}J_i \in \mathbb{R}^{6 \times DOF} \f$.
      * @note Call updateModel() before this method.
@@ -57,8 +57,9 @@ public:
     const Eigen::MatrixXd getJacobian(const std::string& linkName);
 
     /**
-     * @brief Compute and get the time derivative of Jacobian matrix at the
-     * frame of the specified link \f$ i \f$, expressed in the base frame.
+     * @brief [Non-blocking] Compute and get the time derivative of Jacobian
+     * matrix at the frame of the specified link \f$ i \f$, expressed in the
+     * base frame.
      * @param[in] linkName Name of the link to get Jacobian derivative for.
      * @return Time derivative of Jacobian matrix: \f$ ^{0}\dot{J_i} \in
      * \mathbb{R}^{6 \times DOF} \f$.
@@ -71,8 +72,8 @@ public:
     const Eigen::MatrixXd getJacobianDot(const std::string& linkName);
 
     /**
-     * @brief Compute and get the mass matrix for the generalized coordinates,
-     * i.e. joint space.
+     * @brief [Non-blocking] Compute and get the mass matrix for the generalized
+     * coordinates, i.e. joint space.
      * @return Symmetric positive definite mass matrix: \f$ M(q) \in
      * \mathbb{S}^{DOF \times DOF}_{++} \f$. Unit: \f$ [kgm^2] \f$.
      * @note Call updateModel() before this method.
@@ -80,8 +81,8 @@ public:
     const Eigen::MatrixXd getMassMatrix();
 
     /**
-     * @brief Compute and get the Coriolis/centripetal matrix for the
-     * generalized coordinates, i.e. joint space.
+     * @brief [Non-blocking] Compute and get the Coriolis/centripetal matrix for
+     * the generalized coordinates, i.e. joint space.
      * @return Coriolis/centripetal matrix: \f$ C(q,\dot{q}) \in \mathbb{R}^{DOF
      * \times DOF} \f$.
      * @note Call updateModel() before this method.
@@ -89,8 +90,8 @@ public:
     const Eigen::MatrixXd getCoriolisMatrix();
 
     /**
-     * @brief Compute and get the gravity force vector for the generalized
-     * coordinates, i.e. joint space.
+     * @brief [Non-blocking] Compute and get the gravity force vector for the
+     * generalized coordinates, i.e. joint space.
      * @return Gravity force vector: \f$ g(q) \in \mathbb{R}^{DOF \times 1} \f$.
      * Unit: \f$ [Nm] \f$.
      * @note Call updateModel() before this method.
@@ -98,8 +99,8 @@ public:
     const Eigen::VectorXd getGravityForce();
 
     /**
-     * @brief Compute and get the Coriolis force vector for the generalized
-     * coordinates, i.e. joint space.
+     * @brief [Non-blocking] Compute and get the Coriolis force vector for the
+     * generalized coordinates, i.e. joint space.
      * @return Coriolis force vector: \f$ c(q,\dot{q}) \in \mathbb{R}^{DOF
      * \times 1} \f$. Unit: \f$ [Nm] \f$.
      * @note Call updateModel() before this method.
