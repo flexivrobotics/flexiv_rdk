bdbc324a06540aad6dd5d8ac83d344986d9838c9
Peizhang Zhu <peizhang.zhu@flexiv.com>
2024-08-02 10:21:51 -0700

    Specify -DCMAKE_PREFIX_PATH in thirdparty build scripts

--- thirdparty/scripts/install_tinyxml2.sh
+++ thirdparty/scripts/install_tinyxml2.sh
@@ -25,6 +25,7 @@ cmake .. -DCMAKE_BUILD_TYPE=Release \
          -DBUILD_SHARED_LIBS=OFF \
          -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
          -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+         -DCMAKE_PREFIX_PATH=$INSTALL_DIR
 
 # Build and install
 cmake --build . --target install --config Release -j $NUM_JOBS
