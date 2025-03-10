60eea1b2768234ab2b5801b82c5079dcbb168fac
Peizhang Zhu <peizhang.zhu@flexiv.com>
2025-03-10 15:28:07 -0700

    Remove IDLE mode requirement for FileIO class

--- include/flexiv/rdk/file_io.hpp
+++ include/flexiv/rdk/file_io.hpp
@@ -31,12 +31,11 @@ public:
      * @brief [Blocking] Upload a trajectory file (.traj) to the robot.
      * @param[in] file_dir Relative or absolute path of the directory that contains the file to
      * upload, e.g. /home/user/Documents/. Do not include the file name here.
-     * @param[in] file_name Full name of the trajectory file to upload, including the suffix, e.g.
-     * PolishSpiral.traj. Do not include the directory path here.
+     * @param[in] file_name Full name of the trajectory file to upload, including the extension.
+     * E.g. PolishSpiral.traj. Do not include the directory path here.
      * @throw std::invalid_argument if failed to find or load the specified file.
-     * @throw std::logic_error if robot is not in the correct control mode.
-     * @throw std::runtime_error if failed to transfer the file.
-     * @note Applicable control modes: IDLE.
+     * @throw std::length_error if the file is too large to transfer. Maximum file size is 512 kb.
+     * @throw std::runtime_error if failed to upload the file.
      * @note This function blocks until the file is successfully uploaded.
      */
     void UploadTrajFile(const std::string& file_dir, const std::string& file_name);
