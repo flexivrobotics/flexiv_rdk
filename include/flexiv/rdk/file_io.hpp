38c9e4f12093c8400fe29ab8baea4d402199e770
Peizhang Zhu <peizhang.zhu@flexiv.com>
2025-03-10 15:32:24 -0700

    Add FileIO::traj_files_list() and DownloadTrajFile()

--- include/flexiv/rdk/file_io.hpp
+++ include/flexiv/rdk/file_io.hpp
@@ -13,8 +13,7 @@ namespace rdk {
 
 /**
  * @class FileIO
- * @brief Interface for file transfer with the robot. The robot must be put into IDLE mode when
- * transferring files.
+ * @brief Interface for file transfer with the robot. Only certain types of file can be transferred.
  */
 class FileIO
 {
@@ -28,6 +27,14 @@ public:
     virtual ~FileIO();
 
     /**
+     * @brief [Blocking] Get a list of all trajectory files currently stored in the connected robot.
+     * @return Trajectory filenames as a string list.
+     * @throw std::runtime_error if failed to get a reply from the connected robot.
+     * @note This function blocks until a reply is received.
+     */
+    const std::vector<std::string> traj_files_list() const;
+
+    /**
      * @brief [Blocking] Upload a trajectory file (.traj) to the robot.
      * @param[in] file_dir Relative or absolute path of the directory that contains the file to
      * upload, e.g. /home/user/Documents/. Do not include the file name here.
@@ -39,6 +46,30 @@ public:
      * @note This function blocks until the file is successfully uploaded.
      */
     void UploadTrajFile(const std::string& file_dir, const std::string& file_name);
+
+    /**
+     * @brief [Blocking] Download a trajectory file (.traj) from the robot.
+     * @param[in] file_name Full name of the trajectory file to download, including the extension.
+     * E.g. PolishSpiral.traj.
+     * @return String content of the trajectory file.
+     * @throw std::invalid_argument if the specified file does not exist.
+     * @throw std::runtime_error if failed to download the file.
+     * @note This function blocks until the file is successfully downloaded.
+     */
+    const std::string DownloadTrajFile(const std::string& file_name);
+
+    /**
+     * @brief [Blocking] Download a trajectory file (.traj) from the robot and save to the specified
+     * directory.
+     * @param[in] file_name Full name of the trajectory file to download, including the extension.
+     * E.g. PolishSpiral.traj.
+     * @param[in] save_dir Relative or absolute path of the directory to save the downloaded file
+     * to. E.g. /home/user/Documents/. Do not include the file name here.
+     * @throw std::invalid_argument if the specified file does not exist.
+     * @throw std::runtime_error if failed to download or save the file.
+     * @note This function blocks until the file is successfully downloaded.
+     */
+    void DownloadTrajFile(const std::string& file_name, const std::string& save_dir);
 
 private:
     class Impl;
