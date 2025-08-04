2742e7e16d43cc0d11c02ec5e83d4e80db7af63f
pzhu-flexiv <34045850+pzhu-flexiv@users.noreply.github.com>
2025-08-04 11:02:46 -0700

    Feature/support qnx802 (#122)
    * Add compilation support of QNX 8.0.2 for x86_64 and aarch64le targets
    
    * Update thirdparty build scripts
    
    * Bump version of foonathan_memory_vendor to v1.3.1 to compile for QNX
    
    * Add patch files for eigen and fastdds to compile for QNX
    
    * Add QNX contents to README

--- thirdparty/scripts/install_Fast-DDS.sh
+++ thirdparty/scripts/install_Fast-DDS.sh
@@ -3,10 +3,6 @@
 set -e
 echo "Installing Fast-DDS"
 
-# Get install directory and number of parallel build jobs as script arguments
-INSTALL_DIR=$1
-NUM_JOBS=$2
-
 # Clone source code
 if [ ! -d Fast-DDS ] ; then
   git clone https://github.com/eProsima/Fast-DDS.git
@@ -20,13 +16,15 @@ git fetch -p
 git checkout v2.6.7
 git submodule update --init --recursive
 
+# Apply patch if building for QNX
+git reset --hard
+if [ -n "$QNX_TARGET" ]; then
+  git apply $SCRIPTPATH/patches/fastdds_qnx802.patch
+fi
+
 # Configure CMake
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-         -DBUILD_SHARED_LIBS=OFF \
-         -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-         -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-         -DCMAKE_PREFIX_PATH=$INSTALL_DIR \
+cmake .. $SHARED_CMAKE_ARGS \
          -DTHIRDPARTY_Asio=ON \
          -DCOMPILE_EXAMPLES=OFF \
          -DSQLITE3_SUPPORT=OFF \
--- thirdparty/scripts/install_tinyxml2.sh
+++ thirdparty/scripts/install_tinyxml2.sh
@@ -2,10 +2,6 @@
 set -e
 echo "Installing tinyxml2"
 
-# Get install directory and number of parallel build jobs as script arguments
-INSTALL_DIR=$1
-NUM_JOBS=$2
-
 # Clone source code
 if [ ! -d tinyxml2 ] ; then
   git clone https://github.com/leethomason/tinyxml2.git
@@ -17,15 +13,10 @@ fi
 # Use specific version
 git fetch -p
 git checkout 8.0.0
-git submodule update --init --recursive
 
 # Configure CMake
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-         -DBUILD_SHARED_LIBS=OFF \
-         -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-         -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-         -DCMAKE_PREFIX_PATH=$INSTALL_DIR
+cmake .. $SHARED_CMAKE_ARGS
 
 # Build and install
 cmake --build . --target install --config Release -j $NUM_JOBS
