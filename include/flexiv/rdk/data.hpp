a10fc57d61c764e0704cf8e5c36d6afb91585e1d
Peizhang Zhu <peizhang.zhu@flexiv.com>
2026-04-02 16:23:47 -0700

    Update kIOPorts in data.hpp

--- include/flexiv/rdk/data.hpp
+++ include/flexiv/rdk/data.hpp
@@ -25,8 +25,9 @@ constexpr size_t kSerialJointDoF = 7;
 /** Size of pose array (3 position + 4 quaternion) */
 constexpr size_t kPoseSize = 7;
 
-/** Number of digital IO ports (16 on control box + 2 inside the wrist connector) */
-constexpr size_t kIOPorts = 18;
+/** Number of digital IO ports (16 on control box + 2 inside the wrist connector) * maximum 4 wrists
+ */
+constexpr size_t kIOPorts = 16 + 2 * 4;
 
 /** Maximum number of external axes */
 constexpr size_t kMaxExtAxes = 6;
